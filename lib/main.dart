8e9a5018d613da45dd72efd8de27beb9825e4535
21-11089@g.batstate-u.edu.ph <21-11089@g.batstate-u.edu.ph>
2024-07-02 09:38:24 +0800

    Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Home Page Example',
+      title: '',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -17,7 +18,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<String> imgList = [
+    'assets/front.jpg',
+    'assets/Room1.jpg',
+    'assets/Room2.jpg',
+    'assets/Room3.jpg',
+    'assets/view.jpg',
+    'assets/roomview.jpg',
+    'assets/Room4.jpg',
+    'assets/cottage.jpg',
+    'assets/comfortroom.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,20 +51,54 @@ class MyHomePage extends StatelessWidget {
         title: Text('Kandahar Cottages'),
         backgroundColor: Colors.brown[200],
         centerTitle: true,
-        toolbarHeight: 150.0,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          child: SizedBox(
-            width: 900,
-            height: 900,
-            child: Image.asset('assets/logo.png'),
-          ),
+        toolbarHeight: 110.0,
+
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.brown[900],
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('My Profile'),
+              onTap: () {
+                // Handle the Search tap
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.history_sharp),
+              title: Text('History'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle_outlined),
+              title: Text('Account'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
+
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          // Second AppBar-like widget
           PreferredSize(
             preferredSize: Size.fromHeight(80.0),
             child: Container(
@@ -51,63 +111,96 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-
-          Padding(
-            padding: const EdgeInsets.all(16.0),
+          Center(
             child: Text(
-              'ROOMS',
-              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-
+              '',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              autoPlay: true,
+              enlargeCenterPage: true,
+            ),
+            items: imgList.map((item) => Container(
+              child: Center(
+                child: Image.asset(item, fit: BoxFit.cover, width: 1000),
+              ),
+            )).toList(),
+          ),
+          Center(
+            child: Text(
+              '',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
           ),
           Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center, // Center contents horizontally
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                    child: Center(
-                      child: Text(
-                        'Container 1',
-                        style: TextStyle(fontSize: 18.0, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 16.0), // Optional spacing between containers
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                    child: Center(
-                      child: Text(
-                        'Container 2',
-                        style: TextStyle(fontSize: 18.0, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 16.0), // Optional spacing between containers
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text(
-                        'Container 3',
-                        style: TextStyle(fontSize: 18.0, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+          child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18.0),
+            child: Text(
+              'The KANDAHAR COTTAGES, owned by Ms. Neneth B. Lejano, and is located '
+                  'at Matabungkay, Lian Batangas. It has 9 rooms, 2 of this is duplex '
+                  'rooms, 3 couple rooms, 1 family rooms good for 20 persons, 1 family room '
+                  'good for 10 persons and a presidential suite. It also have kubo’s that can '
+                  'be rented by barkada’s for gathering and many other more vicinities that can '
+                  'be explored. If the customer wants to explore or enjoy the beach they can '
+                  'rent some aqua facilities like banana boat, sail boat, motor boat, aqua bike '
+                  'and floating raft in the beach. And also they can rent a videoke. About '
+                  'the safety of the customer the Manager of the Kandahar Cottages rent a life guard.',
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
             ),
           ),
-    ],
-              ),
-    );
+          ),
 
+          Center(
+            child: Text(
+              '',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Kandahar Cottages',
+              style: TextStyle(fontSize: 27.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Matabungkay Lian, Batangas',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Center(
+            child: Text(
+              '09057556578',
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.brown[100],
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.night_shelter_outlined),
+            label: 'Acccommodations',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Bookings',
+          ),
+      ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
